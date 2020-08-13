8c5c8cf66d412ec2134a66c3ae6d3460b4f19639
gaurav-12 <gaurav.12apr@gmail.com>
2020-08-13 12:48:06 +0530

    Department Details shows logo of the selected Department now(in Routing Case Study)

--- src/app/routing-casestudy/department-details/department-details.component.ts
+++ src/app/routing-casestudy/department-details/department-details.component.ts
@@ -8,21 +8,28 @@ import { ActivatedRoute, Router } from "@angular/router";
 })
 export class DepartmentDetailsComponent {
   departments = [
-    { id: 0, name: "Angular" },
-    { id: 1, name: "MongoDB" },
-    { id: 2, name: "Express" },
-    { id: 3, name: "Node JS" },
-    { id: 4, name: "Bootstrap" }
+    { id: 0, name: "Angular", url: "https://cdn.jsdelivr.net/gh/gaurav-12/angular-basics-for-beginners@master/src/assets/angular.png" },
+    { id: 1, name: "MongoDB", url: "https://cdn.jsdelivr.net/gh/gaurav-12/angular-basics-for-beginners@master/src/assets/mongodb.png" },
+    { id: 2, name: "Express", url: "https://cdn.jsdelivr.net/gh/gaurav-12/angular-basics-for-beginners@master/src/assets/expressjs.png" },
+    { id: 3, name: "Node JS", url: "https://cdn.jsdelivr.net/gh/gaurav-12/angular-basics-for-beginners@master/src/assets/nodejs.png" },
+    { id: 4, name: "Bootstrap", url: "https://cdn.jsdelivr.net/gh/gaurav-12/angular-basics-for-beginners@master/src/assets/bootstrap.png" }
   ];
 
   name = null;
+  logo = null;
 
   constructor(private router: Router, private route: ActivatedRoute) {
     const department = this.departments[this.route.snapshot.paramMap.get("id")];
 
     console.log(department === null, department === undefined);
 
-    if (department === undefined) this.name = "na";
-    else this.name = department.name;
+    if (department === undefined) {
+      this.name = "na";
+      this.logo = "https://cdn.jsdelivr.net/gh/gaurav-12/angular-basics-for-beginners@master/src/assets/404.jpg"
+    }
+    else {
+      this.name = department.name;
+      this.logo = department.url;
+    }
   }
 }
